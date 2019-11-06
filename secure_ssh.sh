cee8ee669fe8f75cdd6fe54f597a63199305e4dc
coldfwind <51349638+coldfwind@users.noreply.github.com>
2019-11-06 08:56:47 +0800

    Create secure_ssh.sh

---
+++ secure_ssh.sh
@@ -0,0 +1,14 @@
+# xshell  
+#! /bin/bash
+cat /var/log/secure|awk '/Failed/{print $(NF-3)}'|sort|uniq -c|awk '{print $2"="$1;}' > /var/log/black.txt
+for i in `cat  /var/log/black.txt`
+do
+  IP=`echo $i |awk -F= '{print $1}'`
+  NUM=`echo $i|awk -F= '{print $2}'`
+   if [ $NUM -gt 5 ];then
+      grep $IP /etc/hosts.deny > /dev/null
+    if [ $? -gt 0 ];then
+      echo "sshd:$IP:deny" >> /etc/hosts.deny
+    fi
+  fi
+done
